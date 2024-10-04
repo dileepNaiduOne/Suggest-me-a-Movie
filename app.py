6544147b8bcb21eb30c4e2957b72ab277e5b2a34
Dileep S. Patcha <dileepnaiduone@gmail.com>
2024-10-04 23:52:27 +0530

    Added Posters

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import google.generativeai as genai
 from langchain import PromptTemplate, LLMChain
 from langchain_google_genai import ChatGoogleGenerativeAI
+from imdb import IMDb
+from PIL import Image
+import requests
 
 
 # Setup Local Environment
@@ -37,8 +40,25 @@ llm = ChatGoogleGenerativeAI(model="gemini-pro", api_key = os.getenv("GOOGLE-API
 llm_chain = LLMChain(prompt=demo_template, llm=llm)
 if len(user_input) != 0:
     suggestion = llm_chain.run(user_input)
-    # print(suggestion)
-    # print(re.findall(r"\d+\.\s+(.+)", suggestion))
+    suggestion_list = re.findall(r"\d+\.\s+(.+)", suggestion)
+    print(suggestion_list)
 
-    st.write(suggestion)
+    st.write(f"WoW!!! This is nice movie. And you may also like \n\n{suggestion}\n\nPlease wait, Getting the Posters... ")
+
+    image_link_list = []
+    for movie in suggestion_list:
+        ia = IMDb()
+
+        movie_object = ia.search_movie(movie)
+        id = movie_object[0].getID()
+        movie = ia.get_movie(id)
+        img_url = movie['cover url']
+
+        image_link_list.append(Image.open(requests.get(img_url, stream=True).raw))
+
+    cols = st.columns(3) 
+    cols[0].image(image_link_list[0], width=200)
+    cols[1].image(image_link_list[1], width=200)
+    cols[2].image(image_link_list[2], width=200)
+
 
